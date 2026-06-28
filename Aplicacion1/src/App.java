d7882dff7435a2f608f66a80410a7b2bdf0a696a
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-06-28 02:00:38 -0400

    Creación de la ventana con título

--- Aplicacion1/src/App.java
+++ Aplicacion1/src/App.java
@@ -1,5 +1,21 @@
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
+    }
+
+    @ Override
+    public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+
+        Scene scene = new Scene(root, 400, 300);
+        
+        primaryStage.setTitle("Registro de Visitantes");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
