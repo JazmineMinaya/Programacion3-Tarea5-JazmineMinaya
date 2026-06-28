f6160da6766ccec34ed81fed14a287d595c23c38
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-06-28 03:12:40 -0400

    Agregando campos para escribir en la interfaz

--- Aplicacion1/src/App.java
+++ Aplicacion1/src/App.java
@@ -1,7 +1,8 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 
 public class App extends Application {
     public static void main(String[] args) throws Exception {
@@ -10,12 +11,44 @@ public class App extends Application {
 
     @ Override
     public void start(Stage primaryStage) {
-        StackPane root = new StackPane();
+        VBox root = new VBox(10);
 
-        Scene scene = new Scene(root, 400, 300);
-        
+        TextField nombre = new TextField();
+        nombre.setPromptText("Jazmine Minaya Peralta");
+        nombre.setMaxWidth(375);
+
+        TextField cedula = new TextField();
+        cedula.setPromptText("001-1234567-8");
+        cedula.setMaxWidth(375);
+
+        TextField telefono = new TextField();
+        telefono.setPromptText("809 000 0000");
+        telefono.setMaxWidth(375);
+
+        TextField motivoVisita = new TextField();
+        motivoVisita.setPromptText("Escriba el motivo de su visita");
+        motivoVisita.setMaxWidth(375);
+
+        TextField personaVisitar = new TextField();
+        personaVisitar.setPromptText("Melody Santos Acevedo");
+        personaVisitar.setMaxWidth(375);
+
+        TextField fecha = new TextField();
+        fecha.setPromptText("DD/MM/AAAA");
+        fecha.setMaxWidth(375);
+
+        root.getChildren().add(nombre);
+        root.getChildren().add(cedula);
+        root.getChildren().add(telefono);
+        root.getChildren().add(motivoVisita);
+        root.getChildren().add(personaVisitar);
+        root.getChildren().add(fecha);
+
+        Scene scene = new Scene(root, 400, 400);
+
         primaryStage.setTitle("Registro de Visitantes");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
+
