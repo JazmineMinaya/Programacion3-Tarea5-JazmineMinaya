bc5e07d95f71bded9491f20574d46b0a01349120
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-06-28 16:57:58 -0400

    Agregando GridPane para organizar los campos en la intefaz y Scene

--- Aplicacion3/src/App.java
+++ Aplicacion3/src/App.java
@@ -4,9 +4,12 @@ import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.ComboBox;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 
 public class App extends Application {
     public static void main(String[] args) throws Exception {
@@ -16,6 +19,11 @@ public class App extends Application {
     @ Override
     public void start(Stage primaryStage) {
         VBox root = new VBox(10);
+
+        GridPane formulario = new GridPane();
+        formulario.setPadding(new Insets(15));
+        formulario.setHgap(10);
+        formulario.setVgap(10);
 
         Label titulo = new Label("Perfil de Estudiante");
         titulo.setStyle("-fx-font-size:14px; -fx-fonx-weight:bold;");
@@ -63,5 +71,48 @@ public class App extends Application {
         HBox contenedorBotones = new HBox(10);
         contenedorBotones.setAlignment(Pos.CENTER);
         contenedorBotones.getChildren().addAll(botonGuardar, botonActualizar, botonImprimir);
+
+        double ancho = 350;
+
+        matricula.setPrefWidth(ancho);
+        nombre.setPrefWidth(ancho);
+        carrera.setPrefWidth(ancho);
+        correo.setPrefWidth(ancho);
+        telefono.setPrefWidth(ancho);
+        direccion.setPrefWidth(ancho);
+        semestre.setPrefWidth(ancho);
+
+        formulario.add(labelMatricula, 0, 0);
+        formulario.add(matricula, 1, 0);
+
+        formulario.add(labelNombre, 0, 1);
+        formulario.add(nombre, 1, 1);
+
+        formulario.add(labelCarrera, 0, 2);
+        formulario.add(carrera, 1, 2);
+
+        formulario.add(labelCorreo, 0, 3);
+        formulario.add(correo, 1, 3);
+
+        formulario.add(labelTelefono, 0, 4);
+        formulario.add(telefono, 1, 4);
+
+        formulario.add(labelDireccion, 0, 5);
+        formulario.add(direccion, 1, 5);
+
+        formulario.add(labelSemestre, 0, 6);
+        formulario.add(semestre, 1, 6);
+
+        formulario.add(contenedorBotones, 0, 8, 2, 1);
+
+        root.setPadding(new Insets(10));
+
+        root.getChildren().addAll(contenedorTitulo, formulario);
+
+        Scene scene = new Scene(root, 470, 580);
+
+        primaryStage.setTitle("Perfil de Estudiante");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
